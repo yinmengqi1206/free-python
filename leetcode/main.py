bc9d613829cf070cc70cf02114bbfa62821ce133
yinmengqi <13121552460@163.com>
2024-04-11 11:19:01 +0800

    -

--- leetcode/main.py
+++ leetcode/main.py
@@ -1,8 +1,8 @@
-from solution import Solution
+import solution
+import random
 
 if __name__ == '__main__':
-    # 创建 Solution 类的实例
-    solution_instance = Solution()
     # twoSum
-    print(solution_instance.twoSum([2,7,11,15],9))
+    print(solution.twoSum([2,7,11,15],9))
+    random.seed()
     pass
--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1,8 +1,6 @@
 from typing import List
 
-
 class Solution:
-    # 两数之和 https://leetcode.cn/problems/two-sum/description/
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashtable = dict()
         for i, num in enumerate(nums):
@@ -10,3 +8,7 @@ class Solution:
                 return [hashtable[target-num],i]
             hashtable[nums[i]] = i
         return []
+
+_inst = Solution()
+# 两数之和 https://leetcode.cn/problems/two-sum/description/
+twoSum = _inst.twoSum
