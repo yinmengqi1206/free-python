eb058acca93937bf69339a9af35b2d6e3fc8eae4
yinmengqi <13121552460@163.com>
2024-04-18 18:08:38 +0800

    两数之和

--- leetcode/main.py
+++ leetcode/main.py
@@ -1,8 +1,23 @@
 import solution
-import random
 
 if __name__ == '__main__':
     # twoSum
     print(solution.twoSum([2,7,11,15],9))
-    random.seed()
+    # addTwoNumbers
+    # 创建链表l1: 2 -> 4 -> 3，表示数字342
+    l1 = solution.ListNode(2)
+    l1.next = solution.ListNode(4)
+    l1.next.next = solution.ListNode(3)
+
+    # 创建链表l2: 5 -> 6 -> 4，表示数字465
+    l2 = solution.ListNode(5)
+    l2.next = solution.ListNode(6)
+    l2.next.next = solution.ListNode(4)
+
+    # 调用addTwoNumbers函数
+    # 打印结果链表
+    result = solution.addTwoNumbers(l1, l2)
+    while result:
+        print(result.val, end=" ")
+        result = result.next
     pass
--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1,6 +1,30 @@
-from typing import List
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 
 class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            sum_val = carry
+            if l1:
+                sum_val += l1.val
+                l1 = l1.next
+            if l2:
+                sum_val += l2.val
+                l2 = l2.next
+            
+            carry, val = divmod(sum_val, 10)
+            curr.next = ListNode(val)
+            curr = curr.next
+        return dummy.next
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashtable = dict()
         for i, num in enumerate(nums):
@@ -12,3 +36,5 @@ class Solution:
 _inst = Solution()
 # 两数之和 https://leetcode.cn/problems/two-sum/description/
 twoSum = _inst.twoSum
+# 两数相加 https://leetcode.cn/problems/add-two-numbers/
+addTwoNumbers = _inst.addTwoNumbers
