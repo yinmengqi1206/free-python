f5d74345156f2df0c37259c857f793140b326308
yinmengqi <13121552460@163.com>
2024-04-18 18:14:48 +0800

    两数之和

--- leetcode/main.py
+++ leetcode/main.py
@@ -3,21 +3,16 @@ import solution
 if __name__ == '__main__':
     # twoSum
     print(solution.twoSum([2,7,11,15],9))
-    # addTwoNumbers
-    # 创建链表l1: 2 -> 4 -> 3，表示数字342
-    l1 = solution.ListNode(2)
-    l1.next = solution.ListNode(4)
-    l1.next.next = solution.ListNode(3)
-
-    # 创建链表l2: 5 -> 6 -> 4，表示数字465
-    l2 = solution.ListNode(5)
-    l2.next = solution.ListNode(6)
-    l2.next.next = solution.ListNode(4)
-
-    # 调用addTwoNumbers函数
-    # 打印结果链表
-    result = solution.addTwoNumbers(l1, l2)
+    # 调用solution.addTwoNumbers 并打印返回结果，参数是listNode，循环打印result的数据
+    # l1 = 2 -> 4 -> 3
+    # l2 = 5 -> 6 -> 4
+    result = solution.addTwoNumbers(
+        l1=solution.ListNode(2, solution.ListNode(4, solution.ListNode(3, None))),
+        l2=solution.ListNode(5, solution.ListNode(6, solution.ListNode(4, None)))
+    )
+    # 循环打印结果
     while result:
-        print(result.val, end=" ")
+        # 打印每个node的值
+        print(result.val)
+        # 移动到下一个node
         result = result.next
-    pass
